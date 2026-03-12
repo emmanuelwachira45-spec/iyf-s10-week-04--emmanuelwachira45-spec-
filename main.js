e1df872a6e438c024de08d6c33d8150355ba2df8
emmanuelwachira45-spec <emmanuelwachira45@gmail.com>
2026-03-12 16:34:58 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
+// Variable declarations
+let name = "Your Name";Emmanuel wachira
+let age = 19;
+const birthYear = 2006;
 
+// typeof operator
+console.log(typeof name);    // string
+console.log(typeof age);     // number
+console.log(typeof true);    // boolean
+
+// let vs const
+let score = 100;
+score = 150;  // Works!
+console.log("Score:", score); // Score: 150
+
+const PI = 3.14159;
+// PI = 3;  // Error! const cannot be reassigned
+console.log("PI:", PI); // PI: 3.14159
+
+// Exercise: Variable Practice
+// Declare variables for the following:
+
+// Your name (string)
+let myName = "Emmanuel Wachira";
+console.log("My name is:",Emmanuel wachira);
+
+// Your age (number)
+let myAge = 19;
+console.log("My age is:", 19);
+
+// Whether you're a student (boolean)
+let isStudent = true;
+console.log("Am I a student?:", isStudent);
+
+// Your favorite colors (array - we'll learn more next lesson)
+let favoriteColors = ["blue", "green", "purple"];
+console.log("My favorite colors are:", favoriteColors);
+
+// Today's date (use new Date())
+let todayDate = new Date(3/12/2026);
+console.log("Today's date is:", 3/12/2026);
